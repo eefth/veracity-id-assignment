438a117eb372a61b14fef078cab96227693dc778
Efthimios Efthimiou (efef271045) <extern.Efthimios.Efthimiou@elektrobit.com>
2020-11-29 12:28:29 +0200

    added error handler to RestTemplate

--- src/main/java/com/veracityid/assignment/config/RestTemplateConfiguration.java
+++ src/main/java/com/veracityid/assignment/config/RestTemplateConfiguration.java
@@ -1,10 +1,18 @@
 package com.veracityid.assignment.config;
 
+import java.io.IOException;
+
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -13,56 +21,54 @@ public class RestTemplateConfiguration {
 	@Bean
     public RestTemplate restTemplateForGoogleApi()
     {
-//        CryptoKitDetails details = config.getCryptoKitDetails();
-//
-//        if (isBlank(details.getBaseUrl()))
-//        {
-//            return null;
-//        }
-
-        // set the ssl context
-//        SSLConnectionSocketFactory sslConnectionSocketFactory = null;
-//
-//        SSLContextBuilder sslContextBuilder = new SSLContextBuilder();
-//        sslContextBuilder.setProtocol(SSLConnectionSocketFactory.TLS);
-//
-//        try (FileInputStream fis = new FileInputStream(details.getKeystoreLocation()))
-//        {
-//            KeyStore clientStore = KeyStore.getInstance(details.getKeystoreType());
-//            clientStore.load(fis, details.getKeystorePassword().toCharArray());
-//
-//            sslContextBuilder.loadKeyMaterial(clientStore, details.getKeystorePassword().toCharArray());
-//
-//            sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContextBuilder.build());
-//        }
-//        catch (Exception e)
-//        {
-//            LOG.error("Error in CryptoKit keystore: {}", e.getMessage(), e);
-//
-//            throw new DpcException(message.getCryptoKitKeystoreError(), TivoliMarkers.CRYPTOKIT, e.getMessage());
-//        }
 
         HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-//        clientBuilder.setSSLSocketFactory(sslConnectionSocketFactory);
-//        clientBuilder.useSystemProperties();
-//        if (StringUtils.isNotBlank(config.getProxyHost()))
-//        {
-//            clientBuilder.setProxy(new HttpHost(config.getProxyHost(), config.getProxyPort()));
-//        }
 
         CloseableHttpClient client = clientBuilder.build();
-
 
         // create a request factory
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(client);
 
         RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-        // error handler to deal with errors from CryptoKit
-        //restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
-        //restTemplate.setInterceptors(Collections.singletonList(new RequestLoggingInterceptor(LOG, false)));
-
+        // error handler to deal with errors from Google Api
+        restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
+        
         return restTemplate;
+    }
+	
+	/**
+     * Error handler for responses from CryptoKit
+     */
+    private class RestTemplateResponseErrorHandler implements ResponseErrorHandler
+    {
+        private final Logger log = LoggerFactory.getLogger(RestTemplateResponseErrorHandler.class);
+
+        DefaultResponseErrorHandler errorHandler = new DefaultResponseErrorHandler();
+
+        @Override
+        public boolean hasError(ClientHttpResponse httpResponse) throws IOException
+        {
+            return errorHandler.hasError(httpResponse);
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse httpResponse) throws IOException
+        {
+            // raw status code is safe against non-standard status codes
+            int statusCode = httpResponse.getRawStatusCode();
+
+            if (statusCode == HttpStatus.UNAUTHORIZED.value())
+            {
+                throw new RuntimeException("Could not authenticate");
+            }
+
+            log.error("CryptoKit returned with an error response: {} {}", statusCode, httpResponse.getStatusText());
+
+            String googleApiError = "Unknown Google Api error";
+
+            throw new RuntimeException(googleApiError);
+        }
     }
 	
 }
